dd56650565d78e1cd849eca09671a0f15ef0803e
김상은 <tkddmsrla@collabi.co.kr>
2022-07-30 00:49:58 +0900

    spring cloud gateway 간단 예제 적용

--- eureka/src/main/java/com/example/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.example.discoveryservice;
+package com.example.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryserviceApplication {
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryserviceApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
--- gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.discoveryservice;
+package com.example.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscoveryserviceApplicationTests {
+class GatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
