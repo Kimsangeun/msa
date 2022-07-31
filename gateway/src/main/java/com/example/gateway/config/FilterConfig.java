52036ed12e2b5a341d1218e40c544364620113f6
김상은 <tkddmsrla@collabi.co.kr>
2022-07-31 23:57:24 +0900

    spring cloud gateway GlobalFilter 예제 적용

--- gateway/src/main/java/com/example/gateway/config/FilterConfig.java
+++ gateway/src/main/java/com/example/gateway/config/FilterConfig.java
@@ -1,5 +1,6 @@
 package com.example.gateway.config;
 
+import com.example.gateway.filter.CustomFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 //@Configuration
 public class FilterConfig { // yml 설정 대신 filter 클래스에 설정하는 방법
 //    @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder, CustomFilter customFilter) {
         return builder.routes()
                 .route(r -> r.path("/first-service/**")
                         .filters(f -> f.addRequestHeader("first-request", "first-request-header")
