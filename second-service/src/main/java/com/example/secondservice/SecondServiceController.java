d4b7b39728d29ab22c77e854b60fd8c04073e7ca
김상은 <tkddmsrla@collabi.co.kr>
2022-08-01 02:54:15 +0900

    spring cloud gateway, eureka 연동 예제

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,15 +1,23 @@
 package com.example.secondservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/second-service")
 public class SecondServiceController {
+
+    final Environment env;
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second Service.";
@@ -22,7 +30,9 @@ public class SecondServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from Second Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server Port={}", request.getServerPort());
+        return String.format("Hi, there. This is a message from Second Service on PORT $s."
+                        , env.getProperty("local.server.port"));
     }
 }
